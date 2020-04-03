c28806d45ce2a0744fdd908b99623bad2e296936
yeoulab <44544935+yeoulab@users.noreply.github.com>
2020-04-03 20:50:26 +0900

    Create flaskServer.py

---
+++ flaskServer.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+import requests
+from bs4 import BeautifulSoup
+from flask import jsonify
+from flask_cors import CORS
+import getSise
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+def test():
+    return "test"
+
+@app.route("/unitPrice")
+def getUnitPrice():
+    print(request.args)
+    item_code = request.args.get("item_code")
+    start_date = request.args.get("start_date")
+    print("start_date : {}".format(start_date))
+    dic_res = getSise.getSise(item_code, start_date)
+    response = jsonify(dic_res)
+    response.headers.add("Access-Control-Allow-Credentials", "true")
+    return response
+
+@app.route("/news/naver")
+def getNewsFromNaver():
+    res = requests.get('https://news.naver.com/', verify='C:/SDS.crt')
+    soup = BeautifulSoup(res.content, 'html.parser')
+
+    link_title = soup.find_all('a')
+    #return link_title
+    for num in range(len(link_title)):
+        print(link_title[num].get_text().strip())
+
+if __name__ =="__main__":
+    app.run()
